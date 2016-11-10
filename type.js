500d53c7a3b3fb547bd0b368c0690f6b97de0c00
Naomi Peate <x16512066@student.ncirl.ie>
2016-11-10 13:42:45 +0000

    type.js

---
+++ type.js
@@ -0,0 +1,9 @@
+var myString = "naomi";
+var myNumber = 19;
+var myBoolean = false;
+
+
+// Do not edit the code under this line
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
